37e91c565e5bc536b756c808dc79b84fcc260506
Alexander Schepanovski <suor.web@gmail.com>
2013-10-24 13:16:08 +0800

    Added massive TODO

--- reform.py
+++ reform.py
@@ -10,3 +10,14 @@ import sublime, sublime_plugin
 class ReformCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         self.view.insert(edit, 0, "Hello, World!")
+
+# TODO
+#  - Move functions up and down
+#  - Break long lines
+#  - Reform dicts (object literals) from one-line to multi-line and back
+#  - Same for calls, calls with keyword arguments, array literals
+#  - Break long strings, several variants including switching to multiline separators
+#  - Align =, =>, :, \ and other punctuation
+#  - Reform multiline list, set, dict comprehensions and generator expressions
+#  - Reform for loop to list comprehension
+#  - Switch brackets - parentheses - whatever
