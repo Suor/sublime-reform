b400e31a05e69b43404319c42a4365808b7c1c71
Alexander Schepanovski <suor.web@gmail.com>
2013-10-27 13:07:16 +0800

    Move cursor/selection when swapping words

--- viewtools.py
+++ viewtools.py
@@ -32,12 +32,30 @@ def word_after(view, pos):
     start = view.find_by_class(pos, True, sublime.CLASS_WORD_START)
     return view.word(start)
 
+
 def swap_regions(view, edit, region1, region2):
+    assert region1.b <= region2.a
+
+    # hide selection before move and save shifted position to set it after
+    sel = view.sel()
+    regions = []
+    for region in sel:
+        if region1.contains(region):
+            sel.subtract(region)
+            regions.append(shifted_region(region, region2.b - region1.b))
+
+    # swap text
     str1 = view.substr(region1)
     str2 = view.substr(region2)
     view.replace(edit, region2, str1)
     view.replace(edit, region1, str2)
 
+    # set cursor position/selection to match moved regions
+    sel.add_all(regions)
+
+
+def shifted_region(region, shift):
+    return sublime.Region(region.a + shift, region.b + shift)
 
 
 # def word_at_pos(view, pos=None):
